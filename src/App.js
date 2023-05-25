2a2def4f8a45c41f853c93daf2b337f42c863e71
Ahtsham Ul Haq <112152384+ahtshamuhaq@users.noreply.github.com>
2023-05-25 08:28:51 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
           className="w-full px-4 py-2 text-white bg-indigo-500 rounded hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
           onClick={fetchData}
         >
-          Fetch User Data
+          Fetch Users Data
         </button>
       </div>
 
